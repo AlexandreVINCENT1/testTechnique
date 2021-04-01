edf16962d83693795dedb0c9271f04b60c760cc1
alexandre1VINCENT <alexandre1.vincent@epitech.eu>
2021-04-01 22:18:53 +0200

    correct fix command.sh and add help flag

--- command.sh
+++ command.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-function build () {
-    export FLASK_APP=server/app.py
+function build ()
+{
     pip install -r ./server/requirements.txt
     cd client
     npm i
 }
 
-function launch () {
+function launch ()
+{
     cd client
     npm start &
     cd ..
+    export FLASK_APP=server/app.py
     python3 -m flask run -h localhost -p 8080
 }
 
 if [ $1 == "install" ]
 then
-    build()
+    build
     echo "Client and Server installed"
-
 elif [ $1 == "launch" ]
 then
     echo "Starting Client and Server"
-    launch()
+    launch
 elif [ $1 == "-h" ]
 then
     echo "commands: install, launch or -h"
