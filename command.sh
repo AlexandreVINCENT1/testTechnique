09361ea65942f2dc217fd309cbb602caec1a062b
alexandre1VINCENT <alexandre1.vincent@epitech.eu>
2021-04-01 21:57:18 +0200

    fix script

--- command.sh
+++ command.sh
@@ -2,6 +2,7 @@
 
 function build () {
     export FLASK_APP=server/app.py
+    pip install -r ./server/requirements.txt
     cd client
     npm i
 }
@@ -13,11 +14,18 @@ function launch () {
     python3 -m flask run -h localhost -p 8080
 }
 
-if [ $1 == "install" ]; then
+if [ $1 == "install" ]
+then
     build()
     echo "Client and Server installed"
 
-elif [ $1 == "launch" ]; then
+elif [ $1 == "launch" ]
+then
     echo "Starting Client and Server"
     launch()
+elif [ $1 == "-h" ]
+then
+    echo "commands: install, launch or -h"
+else
+    echo "Bad arguments"
 fi
